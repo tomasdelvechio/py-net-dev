474f1f327a19cb36206e256f9c18fc7e98e5fced
Tomas <tomasdelvechio17@gmail.com>
2013-09-17 18:37:46 -0300

    daytime protocol TCP version completed

--- Protocolos/daytime/daytime_client_stream.py
+++ Protocolos/daytime/daytime_client_stream.py
@@ -5,21 +5,23 @@ import socket
 import sys
 
 if(len(sys.argv) < 2):
-    sys.exit("Forma de ejecucion: python %s <server_ip> <server_port> [<timezone>]" % sys.argv[0])
+    sys.exit("Forma de ejecucion: python %s <server_ip> <server_port>" % sys.argv[0])
 
 if(len(sys.argv) < 3):
-    sys.exit("Falta <server_port>. Forma de ejecucion: python %s <server_ip> <server_port> [<timezone>]" % sys.argv[0])
+    sys.exit("Falta <server_port>. Forma de ejecucion: python %s <server_ip> <server_port>" % sys.argv[0])
 
-if(len(sys.argv) < 4):
-    tz = "0"
-else:
-    tz = sys.argv[3]
+buff_size = 4096
+host, port = sys.argv[1],int(sys.argv[2])
 
-buff_size = 4096                # Tamaño del buffer
-host, port = sys.argv[1],int(sys.argv[2])  # Host y Puerto a donde se va a conectar
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Declaración del Socket
-s.connect((host, port))         # Inicia la conexión TCP contra el servidor
-s.send(tz)                  # Envío algo
-data = s.recv(buff_size)               # Recibo algo
-print "Fecha y Hora: ", data
-s.close()                       # Cierro el socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((host, port))
+s.send("0")
+data = s.recv(buff_size)
+
+dateinfo,tz,formato,version = data.splitlines()
+print "Fecha y Hora: ", dateinfo
+print "Zona horaria: ",tz
+print "Formato: ", formato
+print "Protocol Version: ", version
+
+s.close()
--- Protocolos/daytime/daytime_server_stream.py
+++ Protocolos/daytime/daytime_server_stream.py
@@ -2,35 +2,45 @@
 # -*- coding: utf-8 -*-
 
 import socket
-#~ from time import sleep
 import datetime
 import sys
+import pytz
 
 if(len(sys.argv) < 2):
-    sys.exit("Forma de ejecucion: python %s <server_ip> <server_port>" % sys.argv[0])
+    sys.exit("Forma de ejecucion: python %s <server_ip> <server_port> <timezone>. Para informacion sobre las TimeZone disponibles consultar el archivo TIMEZONE." % sys.argv[0])
 
 if(len(sys.argv) < 3):
-    sys.exit("Falta <server_port>. Forma de ejecucion: python %s <server_ip> <server_port>" % sys.argv[0])
+    sys.exit("Falta <server_port>. Forma de ejecucion: python %s <server_ip> <server_port> <timezone>" % sys.argv[0])
 
-buffsize = 4096                # Tamaño del buffer
-host, port = sys.argv[1],int(sys.argv[2])  # Host y Puerto a donde se va a conectar
-backlog = 5                     # Cantidad de conexiones que vamos a atender/encolar
+if(len(sys.argv) < 4):
+    sys.exit("Falta <timezone>. Forma de ejecucion: python %s <server_ip> <server_port> <timezone>" % sys.argv[0])
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   # Declaración del Socket TCP
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # Permite reutilizar host/port "inmediatamente" si se nos cae el servidor
-s.bind((host, port))            # Le indico en qué host y puerto va a atender las peticiones
-s.listen(backlog)               
+buffsize = 1 # Espero un paquete de 1 byte
+host, port = sys.argv[1],int(sys.argv[2])
+backlog = 5 # Cantidad de conexiones que vamos a atender/encolar
+tz = sys.argv[3]
+fmt = '%A, %B %d, %Y %H:%M:%S-%Z' # Formato con el que se envia el datetime al cliente
+version = '1.0.0'
 
-while True:                    # Bucle principal del servidor
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind((host, port))
+s.listen(backlog)
+
+while True:
     client_sock, client_addr = s.accept() # Acepta conexiones y retorna el sock para llegar al cliente y su dirección
     print "Conexion desde:", client_sock.getpeername()
-    timezone = client_sock.recv(buffsize)  # Leo los datos entrantes
-    while len(timezone):                  # Mientras haya datos sigo leyendo
-        utc_datetime = datetime.datetime.utcnow()
-        delta = datetime.timedelta(hours = int(timezone))
-        data = str(utc_datetime + delta)
-        print "    Respuesta:", data
-        client_sock.sendall(data)
-        timezone = client_sock.recv(buffsize) # Sigo leyendo el buffer hasta que este vacío y salga del while
+    request = client_sock.recv(buffsize)  # Leo los datos entrantes
+    
+    # Calcula la hora segun la timezone que se determino al levantar el servidor
+    utc_datetime = datetime.datetime.utcnow()
+    utc_datetime = utc_datetime.replace(tzinfo=pytz.utc)
+    data = datetime.datetime.astimezone(utc_datetime, pytz.timezone(tz))
+    
+    response =  """%s\n%s\n%s\n%s""" % (str(data.strftime(fmt)),tz,fmt,version) # Arma la estructura de respuesta para el cliente.
+    client_sock.sendall(response)
+    
+    print "    Respuesta:", data
     print "Conexion cerrada por:", client_sock.getpeername()
+    
     client_sock.close()
