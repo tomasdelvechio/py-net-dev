f4bb513c844f83bc5492197ef53fb57298108f9e
Tomas <tomasdelvechio17@gmail.com>
2013-09-23 23:19:54 -0300

    Creada clase de server de sesion remota. Manejo basico de sesiones a medio implementar. Falta crear la clase cliente.

--- Protocolos/remote session/remote_session_client.py
+++ Protocolos/remote session/remote_session_client.py
@@ -3,26 +3,36 @@
 
 import socket
 import sys
+import json
 
 #~ if(len(sys.argv) < 2):
     #~ sys.exit("Falta el mensaje. Forma de ejecucion: python %s mensaje" % sys.argv[0])
 #~ 
 #~ data_send = sys.argv[1]
+buff_size = 4096
+host, port = 'localhost', 8022
 
-cmd = raw_input('user@serverhost:$ ') # ToDo: Esto debe venir del server
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((host, port))
 
+# Solicita login plano
+request = { "opt": {"login":"tomas"} }
+
+s.send(json.dumps(request))
+
+response = json.loads(s.recv(buff_size))
+prompt = response["options"]["prompt"]
+
+cmd = ''
 while cmd != "exit":
-    buff_size = 4096
-    host, port = 'localhost', 8022
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((host, port))
+    cmd = raw_input(prompt)
     s.send(cmd)
     response = s.recv(buff_size)
     while 1:
         data = s.recv(buff_size)
         response += data
         if not data: break
-
     print response
-    s.close()
-    cmd = raw_input('user@serverhost:$ ')
+
+s.close()
+#~ cmd = raw_input('user@serverhost:$ ')
--- Protocolos/remote session/remote_session_server.py
+++ Protocolos/remote session/remote_session_server.py
@@ -3,39 +3,140 @@
 
 import socket
 import sys
+import json
+from os.path import expanduser
 from subprocess import call
+import random
 
-if(len(sys.argv) < 2):
-    sys.exit("Forma de ejecucion: python %s <server_ip> <server_port>" % sys.argv[0])
-
-if(len(sys.argv) < 3):
-    sys.exit("Falta <server_port>. Forma de ejecucion: python %s <server_ip> <server_port>" % sys.argv[0])
-
-buffsize = 4096
-host, port = sys.argv[1],int(sys.argv[2])
-backlog = 5
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-s.bind((host, port))
-s.listen(backlog)
-
-while True:
-    client_sock, client_addr = s.accept()
-    print "Conexion desde:", client_sock.getpeername()
-    data = client_sock.recv(buffsize)
-    #~ while len(data):
-    #~ print "Comando: ",data
-    with open('outfile','w') as fout:
-        call(data,stdout=fout,shell=True)
-    response = open('outfile').read()
-    #~ print response
-    client_sock.sendall(response)
+#~ def generarPrompt(login = None):
     
-    #~ client_sock.sendall(data)
-    #~ data = client_sock.recv(buffsize) # Sigo leyendo el buffer hasta que este vacío y salga del while
+    #~ home = expanduser("~")
     
-    # Cuando termino de llegar el comando, lo ejecuta
+class RSessionServer:
     
-    print "Conexion cerrada por:", client_sock.getpeername()
-    client_sock.close()
+    def __init__(self,host='localhost',port=8022,buffsize=4096,backlog=5,usersFile='users'):
+        self.sessions = {} # Sesiones de usuarios
+        self.buffsize = buffsize
+        
+        # Definicion del socket
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.s.bind((host, port))
+        self.s.listen(backlog)
+        
+        # Carga los usuarios validos para loguearse
+        try:
+            # Si quedo una descarga trunca, la limpiamos
+            f = open(usersFile)
+            self.validUsers = json.loads(f.read())
+        except IOError:
+            raise IOError('No se encuentra el archivo de usuarios. Consulte README.rsession.')
+    
+    def validUser(self,user=None):
+        if self.validUsers.has_key(user):
+            return True
+        else:
+            return False
+    
+    def createSession(self,user=None,client_name=('localhost',None)):
+        
+        if self.validUser(user):
+            # Genero la credencial de acceso al server
+            session = {}
+            session['session'] = self._get_session_cred()
+            session['name'] = client_name # Guardamos informacion de (host,port)
+            session['home'] = self._get_default_directory(user)
+            session['directory'] = self._get_default_directory(user) # Cuando inicia sesion, se posiciona en su home
+            session['login'] = True
+            session['user'] = user
+            
+            # Al hacer de clave de sesion al id, hacemos al server multisession
+            #   El unico problema es que la session va a tener que viajar en cada request
+            self.sessions[session['session']] = session
+            
+            return session['session']
+            
+        else:
+            
+            return False
+    
+    def getPrompt(self,session=None):
+        
+        if self.sessions.has_key(session):
+            
+            return "%(user)s@%(hostname)s:%(directory)s$ " % {  'user' : self.sessions[session]['user'], \
+                                                                'hostname' : socket.gethostname(), \
+                                                                'directory' : self.sessions[session]['directory'] }
+        else:
+            return False
+    
+    def _get_session_cred(self):
+        return random.getrandbits(128) # Identificador de session
+    
+    def _get_default_directory(self,user=None):
+        
+        if self.validUser(user):
+            return self.validUsers[user]['home']
+        else:
+            return '/'
+            
+    def load_server(self):
+        
+        print "Servidor iniciado en: ", self.s.getsockname()
+        
+        while True:
+            
+            client_sock, client_addr = self.s.accept()
+            print "Conexion desde:", client_sock.getpeername()
+            data = client_sock.recv(self.buffsize)
+            
+            request = json.loads(data)
+            
+            # Recibido el request, hay que controlar 2 cosas:
+            #   Si viene con opt (opciones) y si viene un cmd (comando)
+            #   Si viene un comando, hay que controlar que la sesion exista
+            
+            response = {}
+            
+            # Manejo de opciones
+            if(request.has_key('opt')):
+                
+                if(request['opt'].has_key('login')):
+                    
+                    session_id = self.createSession(request['opt']['login'])
+                    
+                    if session_id:
+                        response['opt'] = { 'session' : session_id, \
+                                            'prompt' : self.getPrompt(session_id)}
+            
+            # Manejo de comandos
+            if(request.has_key('cmd')):
+                
+                # Si es una operacion de login, ignoro cualquier cmd que me envie
+                if( not request['opt'].has_key('login') ):
+                    with open('outfile','w') as fout:
+                        call(request['cmd'],stdout=fout,shell=True) # Ejecuta el comando
+                    response['out'] = open('outfile').read()
+            
+            client_sock.sendall(json.dumps(response))
+            
+            #~ client_sock.sendall(data)
+            #~ data = client_sock.recv(self.buffsize) # Sigo leyendo el buffer hasta que este vacío y salga del while
+            
+            print "Conexion cerrada por:", client_sock.getpeername()
+            client_sock.close()
+
+if __name__ == "__main__":
+    
+    if(len(sys.argv) < 2):
+        sys.exit("Forma de ejecucion: python %s <server_ip> <server_port>" % sys.argv[0])
+
+    if(len(sys.argv) < 3):
+        sys.exit("Falta <server_port>. Forma de ejecucion: python %s <server_ip> <server_port>" % sys.argv[0])
+    
+    host, port = sys.argv[1],int(sys.argv[2])
+    
+    rsession = RSessionServer(host=host,port=port)
+    rsession.load_server()
+    
+
