d6b190e7d3181a27e77b435378306d7e37577533
Tomas <tomasdelvechio17@gmail.com>
2013-10-08 20:47:44 -0300

    Se termina de adaptar cliente y server DGRAM

--- Protocolos/echo server/echo_client_stream.py
+++ Protocolos/echo server/echo_client_stream.py
@@ -14,13 +14,12 @@ if(len(sys.argv) < 4):
     sys.exit("Falta el puerto del servidor. Forma de ejecucion: python %s mensaje <ip_server> <port_server>" % sys.argv[0])
 
 data_send = sys.argv[1]
-
-# Ejemplo de Socket Cliente TCP
 buff_size = 4096
 host, port = sys.argv[2],int(sys.argv[3])
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((host, port))
 s.sendall(data_send)
+
 data = s.recv(buff_size)
 while len(data) == buff_size:
     data = s.recv(buff_size)
--- Protocolos/echo server/echo_server_dgram.py
+++ Protocolos/echo server/echo_server_dgram.py
@@ -2,14 +2,25 @@
 # -*- coding: utf-8 -*-
 
 import socket
+import sys
+
+if(len(sys.argv) < 2):
+    sys.exit("Forma de ejecucion: python %s <server_ip> <server_port>" % sys.argv[0])
+
+if(len(sys.argv) < 3):
+    sys.exit("Falta <server_port>. Forma de ejecucion: python %s <server_ip> <server_port>" % sys.argv[0])
 
 # Ejemplo de Socket Servidor UDP
-buff_size = 4096                # Tamaño del buffer
-host, port = 'localhost', 5600  # Host y Puerto a donde se va a conectar
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)   # Declaración del Socket UDP
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # Permite reutilizar host/port "inmediatamente" si se nos cae el servidor
-s.bind((host, port))            # Le indico en qué host y puerto va a atender las peticiones
+buffsize = 4096
+host, port = sys.argv[1],int(sys.argv[2])
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind((host, port))
+
+print "Server iniciado en", host, port
 while True:                    # Bucle principal del servidor
-    data, address = s.recvfrom(buff_size) # espera conexiones
+    data, address = s.recvfrom(buffsize) # espera conexiones
+    print "Mensaje desde: ", address
+    print "  Mensaje: ", data
     # Si quisiera enviar una respuesta 
     s.sendto(data, address)
