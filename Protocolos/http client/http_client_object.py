eed73ced9442f9a654770ac1e214e5b5e463d68c
Tomas <tomasdelvechio17@gmail.com>
2013-10-09 20:30:26 -0300

    Se eliminan lineas de test de la clase, ahora innecesarias.

--- Protocolos/http client/http_client_object.py
+++ Protocolos/http client/http_client_object.py
@@ -229,27 +229,3 @@ class HttpClient:
         except:
             return False
         return True
-        
-if __name__=='__main__':
-    # Test...
-    #~ from http_client_object import HttpClient # Forma de importar la clase en un script
-    
-    # Instancia un cliente
-    #~ client = HttpClient() # cliente normal
-    #~ client = HttpClient(proxy='http://proxyw.unlu.edu.ar:8080') # cliente con proxy
-    #~ client = HttpClient(logfile='lalal.txt') # cliente con log personalizado
-    
-    # Recuperacion con HEAD (No descarga el archivo)
-    #~ client.retrieve('http://nesys.com.ar/images/nesys.jpg',method='HEAD')
-    #~ client.retrieve('http://www.tomasdelvechio.com.ar/',method='HEAD')
-    
-    # Recuperacion con GET
-    client.retrieve('http://nesys.com.ar/images/nesys.jpg')
-    #~ client.retrieve('http://www.tyr.unlu.edu.ar')
-    #~ client.retrieve('http://localhost:8000/logounlu-nombre6-sitio-b.png')
-    #~ client.retrieve('http://www.tomasdelvechio.com.ar/')
-    #~ client.retrieve('http://localhost:8000/Mint.vdi')
-    
-    
-
-
