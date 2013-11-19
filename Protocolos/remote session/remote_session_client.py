ca7c23e7714a9b7144182be77e44cddfaefc332f
Tomas <tomasdelvechio17@gmail.com>
2013-11-19 18:32:35 -0300

    Version modificada del cliente y server de rsession. No es funcional, hay problemas en la temporalidad de las llamadas recv.

--- Protocolos/remote session/remote_session_client.py
+++ Protocolos/remote session/remote_session_client.py
@@ -4,46 +4,237 @@
 import socket
 import sys
 import json
+from getpass import getpass
+import rsa
 
-#~ if(len(sys.argv) < 2):
-    #~ sys.exit("Falta el mensaje. Forma de ejecucion: python %s mensaje" % sys.argv[0])
-#~ 
-#~ data_send = sys.argv[1]
-buff_size = 4096
-host, port = 'localhost', 8022
+debug = True
 
-def open_conn(host,port):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((host, port))
-    
-    return s
-    
-# Solicita login plano
-request = { "opt": {"login":"tomas"} }
-s = open_conn(host,port)
-s.send(json.dumps(request))
-response = json.loads(s.recv(buff_size))
-s.close()
+class RSessionError(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
 
-prompt = response["opt"]["prompt"]
-session = response["opt"]["session"]
+class RSessionClient:
 
-cmd = ''
-print "Nueva sesion en %s:%d\n" % (host,port)
-while cmd != "exit":
-    prompt = response["opt"]["prompt"]
-    cmd = raw_input(prompt)
-    if cmd != "exit":
-        request = { 'cmd' : cmd, 'opt' : { 'session' : session } }
-        s = open_conn(host,port)
-        s.send(json.dumps(request))
-        response = s.recv(buff_size)
-        while 1:
-            data = s.recv(buff_size)
-            response += data
-            if not data: break
-        response = json.loads(response)
-        print response['out']
-        s.close()
-print "Sesion cerrada desde %s:%d\n" % (host,port)
-#~ cmd = raw_input('user@serverhost:$ ')
+    def __init__(self, user, password, host, port):
+        
+        # Setea las variables ingresadas por parametro
+        self.user = user
+        self.password = password
+        self.host = host
+        self.port = port
+
+        # Define ctes y otras variables generales
+        self.buff_size = 4096
+        self.srv_pubkey = None
+        self.char_limit = '\n'
+        self.login = False
+        self.prompt = None
+        
+        # Genera las claves publicas y privadas del cliente
+        self.pubkey, self.privkey = self._generate_rsa_keys()
+
+        # Genera el socket de la conexion
+        self.s = self.open_conn()
+
+    def _generate_rsa_keys(self):
+        return rsa.newkeys(512)
+
+    def open_conn(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((self.host, self.port))
+        
+        return s
+
+    def close_conn(self):
+
+        self.s.close()
+
+        return True
+
+    def split_request(self, request, lenght=53):
+
+        chunks = len(request)
+        chunk_size = lenght
+
+        return [ request[i:i+chunk_size] for i in range(0, chunks, chunk_size) ]
+
+
+    def encrypt_message(self, message):
+
+        return rsa.encrypt(message,self.srv_pubkey)
+
+    def decrypt_message(self,encrypted_message):
+
+        return rsa.decrypt(encrypted_message,self.privkey)
+
+    def send_request(self,request):
+
+        encode_request = json.dumps(request) + self.char_limit
+
+        if self.srv_pubkey is None:
+            if debug:
+                print "No tengo clave publica, envio mensaje plano", encode_request
+            self.s.sendall(encode_request)
+            encode_response = self.s.recv(self.buff_size)
+            while True:
+                data = self.s.recv(self.buff_size)
+                encode_response += data
+                if not data: break
+            #encode_response = ''
+            #while True:
+            #    encode_response += self.s.recv(self.buff_size)
+            #    if debug:
+            #        print 'encode_response: ', encode_response
+                # Si el ultimo caracter de la respuesta es el caracter de fin de mensaje, salimos del bucle
+            #    if len(encode_response) > 0:
+            #        if encode_response[-1] == self.char_limit:
+            #            break
+            print encode_response
+            response = json.loads(encode_response)
+            if debug:
+                print "Llego mensaje...."
+        else:
+            if len(encode_request) > 53:
+                splited_request = split_request(encode_request)
+            else:
+                splited_request = [encode_request]
+            
+            for request in splited_request:
+                encrypted_request = encrypt_message(request)
+                self.s.send(encrypted_request)
+
+            encode_response = ''
+            while True:
+                encrypted_response = self.s.recv(self.buff_size)
+                decrypted_response = self.decrypt_message(encrypted_response)
+                encode_response += decrypted_response
+                # Si el ultimo caracter de la respuesta es el caracter de fin de mensaje, salimos del bucle
+                if decrypted_response[-1] == self.char_limit:
+                    break
+
+            response = json.loads(encode_response)
+
+        return response
+
+    # Intercambio de Claves publicas
+    def changes_publickey(self):
+
+        # Armar un request de tipo login, con el nombre de usuario y enviarlo al server
+        request = { 't' : 2, 'pubkey_n' : self.pubkey.n, 'pubkey_e' : self.pubkey.e }
+        response = self.send_request(request)
+        if debug:
+            print "    3 - Llego la respuesta", response
+        if response['t'] == -1:
+            raise RSessionError("Error devuelto por el servidor: %s" % response['error'])
+
+        if response['t'] == 3:
+            # reconstruye la pk del servidor
+            self.srv_pubkey = rsa.PublicKey(response['pubkey_n'],response['pubkey_e'])
+
+    # Proceso de login
+    def get_login(self):
+
+        # Armar un mensaje con user y pass, encriptarlo con la pubkey del server y enviarlo
+        login_request = { 't' : 4, 'user' : user, 'pass' : password }
+        response = self.send_request(host,port,request)
+
+        if response['t'] == '-1':
+            raise RSessionError("Error devuelto por el servidor: %s" % response['error'])
+
+        if response['t'] == '5':
+            # reconstruye la pk del servidor
+            self.login = True
+            if response.has_key("prompt"):
+                self.prompt = response["prompt"]
+            else:
+                self.prompt = '%s@%s:$ ' % (self.user, self.host)
+
+    def run(self):
+
+        # Intercambio de claves
+        self.changes_publickey()
+        if debug:
+            print "     Intercambio clave publica correctamente"
+        # Login
+        if not self.login:
+            self.get_login()
+        print "Nueva sesion en %s:%d\n" % (self.host,self.port)
+
+        # Si login ok, muestro prompt y espero comandos
+        while True:
+
+            cmd = raw_input(self.prompt)
+            
+            # Salimos con el comando exit
+            if cmd == "exit":
+                request = { 't' : 6 }
+            else:
+                request = { 't' : 0, 'cmd' : cmd }
+            
+            response = self.send_request(request)
+
+            if response['t'] == '-1':
+                raise RSessionError("Error devuelto por el servidor: %s" % response['error'])
+
+            if response['t'] == '7':
+                # Cerro sesion correctamente
+                break
+
+            if response['t'] == '1':
+                # reconstruye la pk del servidor
+                self.login = True
+                if response.has_key("prompt"):
+                    self.prompt = response["prompt"]
+
+                print response['out']
+
+        self.close_conn()
+
+        print "Sesion cerrada desde %s:%d\n" % (self.host,self.port)
+
+#######################
+###     MAIN        ###
+#######################
+if __name__ == "__main__":
+
+    # Gestion de parametros y datos de usuario #
+    ############################################
+
+    if(len(sys.argv) < 2):
+        sys.exit("Falta la cadena de conexion. Forma de ejecucion: python %s <user>@<ip_or_name>:[<port>]" % sys.argv[0])
+
+    user,conn_name = sys.argv[1].split('@')
+    host, port = conn_name.split(':')
+
+    if '' in (user,host):
+        sys.exit("Faltan datos en la cadena de conexion. Forma de ejecucion: python %s <user>@<ip_or_name>:[<port>]" % sys.argv[0])
+
+    if port == '':
+        port = 8022
+    else:
+        port = int(port)
+
+    # Solicita el password para el login
+    password = getpass('Password: ')
+
+    # Instancia el objeto #
+    #######################
+
+    client = RSessionClient(user,password,host,port)
+    client.run()
+    #try:
+    #    client.run()
+    #except RSessionError as rse:
+    #    print "Error de aplicacion: ", rse
+    #except Exception as e:
+    #    print "Error general: ", e
+
+    # OLD CODE
+    #response = s.recv(buff_size)
+    #while 1:
+        #data = s.recv(buff_size)
+        #response += data
+        #if not data: break
+    #~ cmd = raw_input('user@serverhost:$ ')
--- Protocolos/remote session/remote_session_server.py
+++ Protocolos/remote session/remote_session_server.py
@@ -6,65 +6,76 @@ import sys
 import json
 from os.path import expanduser
 import os
-#~ from subprocess import call
 import subprocess
 import random
 import shlex
+import rsa
+import select
 
-#~ def generarPrompt(login = None):
-    
-    #~ home = expanduser("~")
-    
+debug = True
+   
 class RSessionServer:
     
     def __init__(self,host='localhost',port=8022,buffsize=4096,backlog=5,usersFile='users'):
         self.sessions = {} # Sesiones de usuarios
         self.buffsize = buffsize
+        self.pubkey, self.privkey = rsa.newkeys(512)
+        self.char_limit = '\n'
         
-        # Definicion del socket
+        # Definicion del socket principal
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.s.bind((host, port))
         self.s.listen(backlog)
+        self.s_input = [self.s, sys.stdin] # Agrega el socket a la lista de sockets a ser controlados por el server
         
         # Carga los usuarios validos para loguearse
+        # 	ToDo: No hay validacion de la estructura y contenido del archivo :/
         try:
-            # Si quedo una descarga trunca, la limpiamos
             f = open(usersFile)
             self.validUsers = json.loads(f.read())
         except IOError:
             raise IOError('No se encuentra el archivo de usuarios. Consulte README.rsession.')
-    
-    def validUser(self,user=None):
-        if self.validUsers.has_key(user):
-            return True
-        else:
-            return False
-    
-    def createSession(self,user=None,client_name=('localhost',None)):
         
-        if self.validUser(user):
-            # Genero la credencial de acceso al server
-            session = {}
-            session['session'] = self._get_session_cred()
-            session['name'] = client_name # Guardamos informacion de (host,port)
-            session['home'] = self._get_default_directory(user)
-            session['directory'] = self._get_default_directory(user) # Cuando inicia sesion, se posiciona en su home
-            session['login'] = True
-            session['user'] = user
-            
-            # Al hacer de clave de sesion al id, hacemos al server multisession
-            #   El unico problema es que la session va a tener que viajar en cada request
-            self.sessions[session['session']] = session
-            
-            return session['session']
-            
+    def validUser(self,user=None,password=None):
+        if self.validUsers.has_key(user):
+            if self.validUsers[user]['pass'] == password:
+                return True
+            else:
+                return False
         else:
-            
             return False
+    
+    def getUserPassword(self,user=None):
+        if self.validUsers.has_key(user):
+            return self.validUsers[user]['pass']
+        else:
+            return False
+        
+    def createSession(self,user=None,password=None,hostname='localhost',port=None,pubkey=None,session=None):
+        # Tengo que diferenciar entre el momento en que me pide conectarse y cuando me manda el login
+        if session is None:
+            # En este caso, recien me envia su clave publica. Datos necesarios: hostname, port, pubkey
+            session = {}
+            session['login'] = False
+            session['pubkey'] = pubkey
+            session['hostname'] = hostname
+            session['port'] = port
+
+            return session
+        else:
+            # Intento de login, con estructura de sesion ya creada. Datos: user, password
+            if self.validUser(user,password):
+                session['home'] = self._get_default_directory(user)
+                session['directory'] = self._get_default_directory(user) # Cuando inicia sesion, se posiciona en su home
+                session['login'] = True
+                session['user'] = user
+
+                return session
+            else:
+                return False
     
     def getPrompt(self,session=None):
-        
         if self.sessions.has_key(session):
             return "%(user)s@%(hostname)s:%(directory)s$ " % {  'user' : self.sessions[session]['user'], \
                                                                 'hostname' : socket.gethostname(), \
@@ -72,73 +83,205 @@ class RSessionServer:
         else:
             return False
     
-    def _get_session_cred(self):
-        return random.getrandbits(128) # Identificador de session
-    
     def _get_default_directory(self,user=None):
-        
         if self.validUser(user):
             return self.validUsers[user]['home']
         else:
             return '/'
     
-    def shellquote(self,s):
-        return "'" + s.replace("'", "'\\''") + "'"
+    def _get_pubkey_package(self):
+        return json.dumps({ 't' : 3, 'pubkey_n' : self.pubkey.n, 'pubkey_e' : self.pubkey.e}) + self.char_limit
+
+    def _get_error_package(self,message):
+        return json.dumps({ 't' : -1, 'error' : message })
+
+    def encrypt_message(self, message, cli_pubkey):
+        return rsa.encrypt(message,cli_pubkey)
+
+    def decrypt_message(self,encrypted_message):
+        return rsa.decrypt(encrypted_message,self.privkey)
+
+    def split_response(self, response, lenght=53):
+        chunks = len(response)
+        chunk_size = lenght
+        return [ response[i:i+chunk_size] for i in range(0, chunks, chunk_size) ]
     
-    def load_server(self):
-        
-        print "Servidor iniciado en: ", self.s.getsockname()
-        
-        while True:
-            client_sock, client_addr = self.s.accept()
-            print "Conexion desde:", client_sock.getpeername()
-            data = client_sock.recv(self.buffsize)
+    def send_response(self, response, s):
+
+        encode_response = json.dumps(response) + self.char_limit
+        sock_name = s.getsockname()
+
+        # Si no esta en el hash de sesiones, entonces se envia en texto plano la rta
+        if not self.sessions.has_key(sock_name):
+            s.send(encode_response)
+        # Si esta en el hash, se envia encriptada
+        else:
+            if len(encode_response) > 53:
+                splited_response = self.split_response(encode_response)
+            else:
+                splited_response = [encode_response]
             
-            request = json.loads(data)
-            
-            # Recibido el request, hay que controlar 2 cosas:
-            #   Si viene con opt (opciones) y si viene un cmd (comando)
-            #   Si viene un comando, hay que controlar que la sesion exista
-            response = {}
-            # Manejo de opciones
-            if(request.has_key('opt')):
-                if(request['opt'].has_key('login')):
-                    session_id = self.createSession(request['opt']['login'])
-                    if session_id:
-                        response['opt'] = { 'session' : session_id, \
-                                            'prompt' : self.getPrompt(session_id)}
-            
-            # Manejo de comandos
-            if(request.has_key('cmd')):
-                # Si es una operacion de login, ignoro cualquier comando que me envie
-                if( not request['opt'].has_key('login') ):
-                    if self.sessions[request['opt']['session']]['login']:
+            for response in splited_response:
+                encrypted_response = encrypt_message(response, self.sessions[sock_name]['pubkey'])
+                s.send(encrypted_response)
+
+        return True
+
+    def _receive_data(self,s):
+        # Evalua el tipo de mensaje y genera el response en consecuencia. Tipos de Mensaje:
+        #       
+        #       0: Request  (cli -> srv)
+        #       1: Response (srv -> cli)
+        #       2: Pubkey intercambio  (cli -> srv)
+        #       3: Pubkey respuesta (srv -> cli)
+        #       4: Encripted user and pass (cli -> srv)
+        #       5: Login response (srv -> cli)
+        #       6: CLose sesion (cli->srv)
+        #      -1: Error (srv -> cli)
+        # Debo fijarme si es un socket de una conexion ya logueada o no
+
+        sock_name = s.getsockname()
+        keep_session = True
+
+        # Si esta en la lista de sesiones ya se conecto al menos una vez, sino, es la primera
+        if self.sessions.has_key(sock_name):
+            # Si esta, hay que distinguir entre solo tener la PubKey y que ya este logueado
+            if self.sessions[sock_name]["login"]:
+                # Recibo mensaje completo
+                encode_request = ''
+                while True:
+                    encrypted_request = s.recv(self.buffsize)
+                    decrypted_request = decrypt_message(encrypted_request)
+                    encode_request += decrypted_request
+                    # Si el ultimo caracter del request es el caracter de fin de mensaje, salimos del bucle
+                    if encode_request[-1] == self.char_limit:
+                        break
+                request = json.loads(encode_request)
+                if request['t'] == 0:
+                    # Mensaje correcto, procesamos comando
+                    if(request.has_key('cmd')):
                         # si es una operacion de cd, entonces cambio la carpeta de trabajo
                         cmd = shlex.split(request['cmd'])
                         if cmd[0] == 'cd':
                             if not os.path.isabs(cmd[1]):
-                                path = os.path.abspath(os.path.join(self.sessions[request['opt']['session']]['directory'],cmd[1]))
+                                path = os.path.abspath(os.path.join(self.sessions[sock_name]['directory'],cmd[1]))
                             else:
                                 path = os.path.abspath(cmd[1])
                             os.chdir(path)
-                            self.sessions[request['opt']['session']]['directory'] = path
+                            self.sessions[sock_name]['directory'] = path
                             response['out'] = ''
                         else:
                             proc = subprocess.Popen(cmd, stdout=subprocess.PIPE,)
                             response['out'] = proc.communicate()[0]
-                        response['opt'] = {}
-                        response['opt']['prompt'] = self.getPrompt(request['opt']['session'])
-            
-            
-            
-            client_sock.sendall(json.dumps(response))
-            
-            #~ client_sock.sendall(data)
-            #~ data = client_sock.recv(self.buffsize) # Sigo leyendo el buffer hasta que este vacío y salga del while
-            
-            print "Conexion cerrada por:", client_sock.getpeername()
-            client_sock.close()
+                        response['prompt'] = self.getPrompt(sock_name)
+                    else:
+                        response['out'] = ''
+                        response['prompt'] = self.getPrompt(sock_name)
+                    response['t'] = 1
+                elif request['t'] == 6:
+                    # Esta cerrando la sesion
+                    self.sessions.pop(sock_name)
+                    response = { 't' : 7 , 'mensaje' : 'OK' }
+                    keep_session = False
+                else:
+                    #Error
+                    response = self._get_error_package("Mensaje mal armado. Se esperaba un tipo de mensaje 0")
+                self.send_response(response, s)
+            else:
+                # Si no esta logueado, esta enviando el mensaje de usuario y login encriptado
+                encode_request = ''
+                while True:
+                    encrypted_request = s.recv(self.buffsize)
+                    decrypted_request = decrypt_message(encrypted_request)
+                    encode_request += decrypted_request
+                    # Si el ultimo caracter de la respuesta es el caracter de fin de mensaje, salimos del bucle
+                    if encode_request[-1] == self.char_limit:
+                        break
+                request = json.loads(encode_request)
+                
+                if request['t'] == 4:
+                    if validUser(request['user'],request['pass']):
+                        # Inicia sesion
+                        self.sessions[sock_name]['home'] = self._get_default_directory(user)
+                        session['directory'] = self._get_default_directory(user) # Cuando inicia sesion, se posiciona en su home
+                        session['user'] = user
+                        session['login'] = True
+                        
+                        response = { 't' : 5 , 'prompt' : self.getPrompt(sock_name) }
+                    else:
+                        #~ Paquete de tipo=-1
+                        response = self._get_error_package("Error al loguearse. Usuario o contraseña incorrectos")
+                else:
+                    response = self._get_error_package("Mensaje mal armado. Se esperaba un tipo de mensaje 4")
+                self.send_response(response,s)
+        else:
+            # Si es la primera vez, me esta enviado de forma plana la PubKey
+            if debug:
+                print "    1 - Primer conexion desde ", sock_name
+            encode_request = ''
+            while True:
+                encode_request += s.recv(self.buffsize)
+                # Si el ultimo caracter de la respuesta es el caracter de fin de mensaje, salimos del bucle
+                if encode_request[-1] == self.char_limit:
+                    break
+            request = json.loads(encode_request)
+            if debug:
+                print "        request trae esto: ", request
+            if request['t'] == 2:
+                if debug:
+                    print "    2 - Mensaje completo llego al server"
+                self.sessions[sock_name] = { 'pubkey' : rsa.PublicKey(request['pubkey_n'],request['pubkey_e']), 'login' : False }
+                print "LALALA"
+                print self._get_pubkey_package()
+                self.s.sendall(self._get_pubkey_package())
+                print "LELELELE"
+            else:
+                response = self._get_error_package("Mensaje mal armado. Se esperaba un tipo de mensaje 2")
 
+        return keep_session
+        
+    def load_server(self):
+        close_server = False
+        print "Servidor iniciado en: ", self.s.getsockname()
+        while True:
+            
+            s_input,s_output,s_error = select.select(self.s_input,[],[])
+            
+            for s in s_input:
+                # Si es el socket del servidor
+                if s == self.s:
+                    c_sock, c_addr = self.s.accept()
+                    print "Conexion inciada desde: %s:%s" % c_sock.getpeername()
+                    self.s_input.append(c_sock)
+                elif s == sys.stdin:
+                    #~ Si el usuario intenta cerrar el servidor
+                    close_server = True
+                    break
+                else:
+                    #~ Es un socket activo
+                    sesion_activa = self._receive_data(s)
+            
+            if close_server:
+                print "Cerrando sockets activos..."
+                for s in s_input:
+                    s.close()
+                self.s.close()
+                print "Saliendo."
+                break
+            
+            # Salvo por un error, envio encriptado el response
+            #if response['t'] in (-1,3):
+                #client_sock.sendall(response)
+            #else:
+                #cypher_response = crypto.encrypt(json.dumps(response),)
+                #client_sock.sendall(cypher_response)
+            
+            #print "Conexion cerrada por:", client_sock.getpeername()
+            #client_sock.close()
+
+#######################
+###     MAIN        ###
+#######################
 if __name__ == "__main__":
     
     if(len(sys.argv) < 2):
